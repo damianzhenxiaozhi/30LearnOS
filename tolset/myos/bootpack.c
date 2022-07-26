9d8cdd03a898b2c54649b55ce471955b26f16479
xiaozhi <tomfan@163.com>
2022-07-26 08:13:15 +0800

    08day

--- tolset/myos/bootpack.c
+++ tolset/myos/bootpack.c
@@ -1,18 +1,12 @@
 #include "bootpack.h"
 
-extern struct FIFO8 keyfifo;
-extern struct FIFO8 mousefifo;
-
-struct KEY_DEC
-{
-	unsigned char data[2], phase;
-};
-
 struct MOUSE_DEC
 {
 	unsigned char buf[3], phase;
+	int x, y, btn;
 };
 
+extern struct FIFO8 keyfifo, mousefifo;
 void wait_KBC_sendready(void);
 void init_keyboard();
 void enable_mouse(struct MOUSE_DEC *mdec);
@@ -23,13 +17,18 @@ void HariMain(void)
 	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
 	char s[40], mcursor[16 * 16], keybuf[32], mousebuf[128];
 	int mx, my, data, i;
-	struct KEY_DEC kdec;
 	struct MOUSE_DEC mdec;
-	unsigned char mouse_debuf[3], mouse_phase;
 
-	init_gdtidt();
-	init_pic();
-	io_sti(); // enable interrupt
+	init_gdtidt(); // gdt idt init
+	init_pic();	   // pic init
+	io_sti();	   // enable interrupt
+
+	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
+	io_out8(PIC0_IMR, 0xf9); // 11111001, bit2->slave, bit1->keyboard
+	io_out8(PIC1_IMR, 0xef); // 11101111, bit12->mouse
+
+	init_keyboard();
 
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -40,14 +39,7 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	fifo8_init(&keyfifo, 32, keybuf);
-	fifo8_init(&mousefifo, 128, mousebuf);
-	io_out8(PIC0_IMR, 0xf9); // 11111001, bit2->slave, bit1->keyboard
-	io_out8(PIC1_IMR, 0xef); // 11101111, bit12->mouse
-
-	init_keyboard();
 	enable_mouse(&mdec);
-	mouse_phase = 0;
 
 	for (;;)
 	{
@@ -70,13 +62,48 @@ void HariMain(void)
 			{
 				data = fifo8_get(&mousefifo);
 				io_sti();
-				i = mouse_decode(&mdec, data);
-				if (i == 1)
+				if (mouse_decode(&mdec, data) != 0)
 				{
-					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0)
+					{
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0)
+					{
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0)
+					{
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 				}
+
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);
+				mx += mdec.x;
+				my += mdec.y;
+				if (mx < 0)
+				{
+					mx = 0;
+				}
+				if (my < 0)
+				{
+					my = 0;
+				}
+				if (mx > binfo->scrnx - 16)
+				{
+					mx = binfo->scrnx - 16;
+				}
+				if (my > binfo->scrny - 16)
+				{
+					my = binfo->scrny - 16;
+				}
+				sprintf(s, "(%3d, %3d)", mx, my);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 10 * 8 - 1, 15);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+				putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 			}
 		}
 	}
@@ -106,11 +133,12 @@ void init_keyboard(void)
 
 void enable_mouse(struct MOUSE_DEC *mdec)
 {
+	mdec->phase = 0;
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	mdec->phase = 0;
+	wait_KBC_sendready();
 	return;
 }
 
@@ -127,9 +155,12 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 
 	if (mdec->phase == 1)
 	{
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
-		return 0;
+		if ((dat & 0xc8) == 0x08)
+		{
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+			return 0;
+		}
 	}
 
 	if (mdec->phase == 2)
@@ -143,6 +174,19 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 	{
 		mdec->buf[2] = dat;
 		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x7;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+
+		if ((mdec->buf[0] & 0x10) != 0)
+		{
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0)
+		{
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = -mdec->y;
 		return 1;
 	}
 
